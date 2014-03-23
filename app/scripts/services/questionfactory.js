dd7211811976259b70ee9b19dfbc52b54cbd6d3d
Robin Skoglund <rsk@knowit.no>
2014-03-23 15:21:31 +0100

    backend-kall ikke kompatibel med isArray ennå

--- app/scripts/services/questionfactory.js
+++ app/scripts/services/questionfactory.js
@@ -1,7 +1,9 @@
 'use strict';
 angular.module('frontendApp').factory('Question', ['$resource', function($resource){
   return $resource('//webtutorialbackend.herokuapp.com/admin/questions/:id', {}, {
-    'query': {method: 'GET', isArray: true},
+    // foreløpig returnerer ikke GET / en array:
+    //'query': {method: 'GET', isArray: true},
+    'query': {method: 'GET'},
     'save': {method: 'POST'}
   });
 }]);
